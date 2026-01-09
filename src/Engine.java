97188ae399d7f2668fbf0ec68e1fe17d5400e72d
Mohamed Medhat <140269335+midog09@users.noreply.github.com>
2026-01-09 13:13:49 +0200

    Update Engine.java

--- src/Engine.java
+++ src/Engine.java
@@ -3,16 +3,14 @@ public class Engine {
     private double engineCapacity;
     private String fuelType;
 
-    // ده اللي كان موجود
     public Engine(double engineCapacity, String fuelType) {
         this.engineCapacity = engineCapacity;
         this.fuelType = fuelType;
     }
 
-    // ضيفنا ده عشان يحل مشكلة الـ Main
     public Engine(double engineCapacity) {
         this.engineCapacity = engineCapacity;
-        this.fuelType = "Petrol"; // قيمة افتراضية
+        this.fuelType = "Petrol";  
     }
 
     public double getEngineCapacity() { return engineCapacity; }
